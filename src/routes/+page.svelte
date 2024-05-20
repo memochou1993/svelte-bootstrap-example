a797c46b41c8e0c2b5d3a94ec7c1ada2ea9e5175
William Chou <williamchou@kklab.com>
2024-05-20 17:06:31 +0800

    Add bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { Tooltip } from 'bootstrap';
+  import { onMount } from 'svelte';
+  import './main.scss';
+
+  let bootstrap: typeof import('bootstrap');
+
+  // enable Bootstrap tooltips
+  onMount(async () => {
+    bootstrap = await import('bootstrap');
+  });
+
+  const handleCopy = () => {
+    // do something
+    tooltip.show();
+    setTimeout(() => tooltip.hide(), 1000);
+  };
+
+  $: tooltip = bootstrap?.Tooltip.getOrCreateInstance('#tooltip') as Tooltip;
+</script>
+
+<button type="button" class="btn btn-primary">Hello, World!</button>
+
+<button
+  class="btn btn-block btn-warning"
+  data-bs-placement="bottom"
+  data-bs-toggle="tooltip"
+  data-bs-trigger="manual"
+  id="tooltip"
+  on:click={handleCopy}
+  title="Clicked Successfully!"
+  type="button"
+>
+  Click!
+</button>
